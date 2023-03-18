116e4270ad8a58aa7608e01c926633557ae71822
dyng <dygvirus@gmail.com>
2023-03-18 21:12:35 +0800

    Add raw field

--- algorithm.go
+++ algorithm.go
@@ -16,9 +16,9 @@ type FeedEntry struct {
 	Id        string    `json:"event_id"`
 	Kind      int       `json:"kind"`
 	Pubkey    string    `json:"pubkey"`
-	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"created_at"`
 	Score     int       `json:"score"`
+	Raw       string    `json:"raw"`
 }
 
 func NewEngine(driver neo4j.DriverWithContext) *Engine {
@@ -36,7 +36,7 @@ func (e *Engine) GetFeed(userPub string, start time.Time, end time.Time, limit i
 	posts, err := session.ExecuteRead(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
 		ctx := context.Background()
 
-		result, err := tx.Run(ctx, "match (p:Post) where p.created_at > $Start and p.created_at < $End optional match (r1:Post)-[:REPLY]->(p) optional match (r2:Post)-[:LIKE]->(p) optional match (r3:Post)-[:ZAP]->(p) with p, count(distinct r1.author)*15+count(distinct r2.author)*10+count(distinct r3.author)*50 as score order by score desc limit $Limit return p.id, p.kind, p.author, p.content, p.created_at, score;",
+		result, err := tx.Run(ctx, "match (p:Post) where p.created_at > $Start and p.created_at < $End optional match (r1:Post)-[:REPLY]->(p) optional match (r2:Post)-[:LIKE]->(p) optional match (r3:Post)-[:ZAP]->(p) with p, count(distinct r1.author)*15+count(distinct r2.author)*10+count(distinct r3.author)*50 as score order by score desc limit $Limit return p.id, p.kind, p.author, p.raw, p.created_at, score;",
 			map[string]any{
 				"Start": start.Unix(),
 				"End":   end.Unix(),
@@ -54,8 +54,8 @@ func (e *Engine) GetFeed(userPub string, start time.Time, end time.Time, limit i
 				Id:        record.Values[0].(string),
 				Kind:      int(record.Values[1].(int64)),
 				Pubkey:    record.Values[2].(string),
-				Content:   record.Values[3].(string),
-				CreatedAt: time.Unix(record.Values[4].(int64), 0),
+				CreatedAt: time.Unix(record.Values[3].(int64), 0),
+				Raw:       record.Values[4].(string),
 				Score:     int(record.Values[5].(int64)),
 			}
 			posts = append(posts, post)
